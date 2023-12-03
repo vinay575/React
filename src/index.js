39a4251ded731e58767f30b74dd5d5ad2c5c0fc6
vinay <bhaskarvinaychitturi@gmail.com>
2023-12-03 11:35:42 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from  'react';
 import ReactDOM from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import image from './images/logo.jpg'
+import image from './images/logo.jpg';
+import './style.css';
 
 
 //(1)create a nested header element using react.create element(h1,h2,h3indide a dive with class "title"
@@ -88,11 +89,11 @@ const Header = () => {
     <>
       <div className='Header'>
         <div className='logo'>
-        <img src={image} alt="Logo" height="100px" width= "100px"/>
+        <img src={image} alt="Logo" height="77px" width= "77px" />
         </div>
         <div className='search'>
-          <input type='text' placeholder='Search Any Thing'></input>
-          <button>Search</button>
+          <input id='search_type' type='text' placeholder='Search Any Thing'></input>
+          <button id='btn'>Search</button>
         </div>
         <div className='icon'>
           <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path fill="#ffde59" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
@@ -102,4 +103,4 @@ const Header = () => {
   );
 };
 
-ReactDOM.render(<Header />, document.getElementById("root"));
+ReactDOM.render(<Header/>, document.getElementById("root"));
